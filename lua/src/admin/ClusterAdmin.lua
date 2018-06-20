5493bf7f4094fb28ddb9503c9316865819494183
xqhuang <xqhuang@chinaums.com>
2018-06-20 11:24:02 +0800

    admin逻辑完成：1. 规则分组 2. 规则 3. 集群 增删改查逻辑编写

--- lua/src/admin/ClusterAdmin.lua
+++ lua/src/admin/ClusterAdmin.lua
@@ -6,44 +6,46 @@ local ErrCode = require("common.ErrCode")
 local Config = require("common.Config")
 
 _M.invokers = {
-    method = "GET",
-    uri = "/clusters",
-    invoke = function(method, uri, params, args)
-        return RouteService.listClusters()
-    end
-}, {
-    method = 'POST',
-    uri = "/clusters",
-    invoke = function(method, uri, params, args)
-        local sizeRlt = RouteService.clusterSize()
-        if not sizeRlt.success then
-            return ErrCode.RULE_DATA_ERROR:detailErrorMsg('查询集群总数失败，无法确认是否超过集群总数限制，插入失败')
-        elseif sizeRlt.data > Config.CLUSTER_MAX then
-            return ErrCode.RULE_DATA_ERROR:detailErrorMsg('添加集群失败，集群总数限制大小为' .. Config.CLUSTER_MAX)
+    {
+        method = "GET",
+        uri = "/clusters",
+        invoke = function(method, uri, params, args)
+            return RouteService.listClusters()
         end
-        return RouteService.addCluster(params)
-    end
-}, {
-    method = 'GET',
-    uri = "(/clusters/(%d+))",
-    invoke = function(method, uri, params, args)
-        return RouteService.queryCluster(args[1])
-    end
-}, {
-    method = 'PUT',
-    uri = "(/clusters/(%d+))",
-    invoke = function(method, uri, params, args)
-        if args[1] ~= params.clusterId then
-            return ErrCode.BAD_REQUEST:detailErrorMsg('请求参数中的分组信息与uri的分组信息不匹配，异常请求')
+    }, {
+        method = 'POST',
+        uri = "/clusters",
+        invoke = function(method, uri, params, args)
+            local sizeRlt = RouteService.clusterSize()
+            if not sizeRlt.success then
+                return ErrCode.RULE_DATA_ERROR:detailErrorMsg('查询集群总数失败，无法确认是否超过集群总数限制，插入失败')
+            elseif sizeRlt.data > Config.CLUSTER_MAX then
+                return ErrCode.RULE_DATA_ERROR:detailErrorMsg('添加集群失败，集群总数限制大小为' .. Config.CLUSTER_MAX)
+            end
+            return RouteService.addCluster(params)
         end
-        return RouteService.updateCluster(params)
-    end
-}, {
-    method = 'DELETE',
-    uri = "(/clusters/(%d+))",
-    invoke = function(method, uri, params, args)
-        return RouteService.deleteCluster(args[1])
-    end
+    }, {
+        method = 'GET',
+        uri = "(/clusters/(%d+))",
+        invoke = function(method, uri, params, args)
+            return RouteService.queryCluster(args[1])
+        end
+    }, {
+        method = 'PUT',
+        uri = "(/clusters/(%d+))",
+        invoke = function(method, uri, params, args)
+            if args[1] ~= params.clusterId then
+                return ErrCode.BAD_REQUEST:detailErrorMsg('请求参数中的分组信息与uri的分组信息不匹配，异常请求')
+            end
+            return RouteService.updateCluster(params)
+        end
+    }, {
+        method = 'DELETE',
+        uri = "(/clusters/(%d+))",
+        invoke = function(method, uri, params, args)
+            return RouteService.deleteCluster(args[1])
+        end
+    }
 }
 
 return _M
--- lua/src/admin/RuleAdmin.lua
+++ lua/src/admin/RuleAdmin.lua
@@ -6,44 +6,46 @@ local ErrCode = require("common.ErrCode")
 local Config = require("common.Config")
 
 _M.invokers = {
-    method = "GET",
-    uri = "(/ruleGroups/(%d+)/rules)",
-    invoke = function(method, uri, params, args)
-        return RouteService.listRules(args[1])
-    end
-}, {
-    method = "POST",
-    uri = "/rules",
-    invoke = function(method, uri, params, args)
-        local sizeRlt = RouteService.ruleSize(params['groupId'])
-        if not sizeRlt.success then
-            return ErrCode.RULE_DATA_ERROR:detailErrorMsg('查询分组下规则总数失败，无法确认是否超过规则总数限制，插入失败')
-        elseif sizeRlt.data > Config.RULE_MAX then
-            return ErrCode.RULE_DATA_ERROR:detailErrorMsg('添加规则失败，集群下规则总数限制大小为' .. Config.RULE_MAX)
+    {
+        method = "GET",
+        uri = "(/ruleGroups/(%d+)/rules)",
+        invoke = function(method, uri, params, args)
+            return RouteService.listRules(args[1])
         end
-        return RouteService.addRule(params)
-    end
-}, {
-    method = "GET",
-    uri = "(/rules/(%d+))",
-    invoke = function(method, uri, params, args)
-        return RouteService.queryRule(args[1])
-    end
-}, {
-    method = "PUT",
-    uri = "(/rules/(%d+))",
-    invoke = function(method, uri, params, args)
-        if args[1] ~= params.ruleId then
-            return ErrCode.BAD_REQUEST:detailErrorMsg('请求参数中的规则信息与uri的规则信息不匹配，异常请求')
+    }, {
+        method = "POST",
+        uri = "/rules",
+        invoke = function(method, uri, params, args)
+            local sizeRlt = RouteService.ruleSize(params['groupId'])
+            if not sizeRlt.success then
+                return ErrCode.RULE_DATA_ERROR:detailErrorMsg('查询分组下规则总数失败，无法确认是否超过规则总数限制，插入失败')
+            elseif sizeRlt.data > Config.RULE_MAX then
+                return ErrCode.RULE_DATA_ERROR:detailErrorMsg('添加规则失败，集群下规则总数限制大小为' .. Config.RULE_MAX)
+            end
+            return RouteService.addRule(params)
         end
-        return RouteService.updateRule(params)
-    end
-}, {
-    method = "DELETE",
-    uri = "(/rules/(%d+))",
-    invoke = function(method, uri, params, args)
-        return RouteService.deleteRule(args[1])
-    end
+    }, {
+        method = "GET",
+        uri = "(/rules/(%d+))",
+        invoke = function(method, uri, params, args)
+            return RouteService.queryRule(args[1])
+        end
+    }, {
+        method = "PUT",
+        uri = "(/rules/(%d+))",
+        invoke = function(method, uri, params, args)
+            if args[1] ~= params.ruleId then
+                return ErrCode.BAD_REQUEST:detailErrorMsg('请求参数中的规则信息与uri的规则信息不匹配，异常请求')
+            end
+            return RouteService.updateRule(params)
+        end
+    }, {
+        method = "DELETE",
+        uri = "(/rules/(%d+))",
+        invoke = function(method, uri, params, args)
+            return RouteService.deleteRule(args[1])
+        end
+    }
 }
 
 return _M
