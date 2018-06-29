7fdda06862009502d953eca47e6b75d42cb7615f
xqhuang <xqhuang@chinaums.com>
2018-06-29 11:10:14 +0800

    地址列表组件

--- route-admin/src/components/addresses-input/addresses-input.vue
+++ route-admin/src/components/addresses-input/addresses-input.vue
@@ -7,12 +7,17 @@
          :name="address"> {{ address }}
     </Tag>
     <Button v-if="editable" icon="ios-plus-empty" type="dashed" size="small" @click="handleAddAddress">添加地址</Button>
-    <Modal v-model="modal" title="提示" @on-ok="handleModalSubmit" @on-cancel="handleModalCancel">
-      <p>ddd</p>
+    <Modal v-model="modal" :footer-hide="true" title="添加地址" @on-cancel="handleModalCancel">
+      <Input v-model="addressInput" :autofocus="true" placeholder="输入 IP:PORT 格式的地址" @on-enter="handleModalSubmit"/>
     </Modal>
   </div>
 </template>
 <script>
+function isValidAddress (address) {
+  var reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]):\d+$$/
+  return reg.test(address)
+}
+
 export default {
   props: {
     value: {
@@ -31,7 +36,8 @@ export default {
   data () {
     return {
       addresses: this.value,
-      modal: false
+      modal: false,
+      addressInput: ''
     }
   },
   watch: {
@@ -56,11 +62,33 @@ export default {
     handleAddAddress () {
       this.modal = true
     },
-    handleModalSubmit () {
-      this.$Message.info('Clicked ok')
+    handleModalSubmit (event) {
+      event.preventDefault()
+
+      if (!isValidAddress(this.addressInput)) {
+        this.$Message.warning('地址格式输入错误')
+        return
+      }
+
+      if (this.addresses === '') {
+        this.addresses = this.addressInput
+        this.modal = false
+        this.addressInput = ''
+        return
+      }
+
+      let array = this.addresses.split(',')
+      if (array.indexOf(this.addressInput) !== -1) {
+        this.$Message.warning('不能添加相同的地址')
+        return
+      }
+      array.push(this.addressInput)
+      this.addresses = array.join(',')
+      this.modal = false
+      this.addressInput = ''
     },
     handleModalCancel () {
-      this.$Message.info('Clicked cancel')
+      this.addressInput = ''
     }
   }
 }
--- route-admin/src/view/single-page/home/home.vue
+++ route-admin/src/view/single-page/home/home.vue
@@ -1,31 +1,8 @@
 <template>
-  <div>
-  <Button type="primary" @click="modal1 = true">Display dialog box</Button>
-  <Modal
-    v-model="modal1"
-    title="Common Modal dialog box title"
-    @on-ok="ok"
-    @on-cancel="cancel">
-    <p>Content of dialog</p>
-    <p>Content of dialog</p>
-    <p>Content of dialog</p>
-  </Modal>
-  </div>
+  <div>home</div>
 </template>
 <script>
 export default {
-  data () {
-    return {
-      modal1: false
-    }
-  },
-  methods: {
-    ok () {
-      this.$Message.info('Clicked ok')
-    },
-    cancel () {
-      this.$Message.info('Clicked cancel')
-    }
-  }
+  name: 'home'
 }
 </script>
